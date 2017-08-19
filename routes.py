f0e797f150e3dc71417e67ea0a357dde657894f3
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2017-08-19 22:07:37 +0530

    Added routes that will render terms and privacy pages.

--- routes.py
+++ routes.py
@@ -5,9 +5,21 @@ from flask import Flask, render_template, request, session, redirect, url_for, a
 app = Flask(__name__)
 app.secret_key = 'f6da2f74842ece5618b5383c71062e12362cbbd5db13cee0'
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
 
 if __name__ == '__main__':
     if 'MODE' in env and env['MODE'] == 'production':
